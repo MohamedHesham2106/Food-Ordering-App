690dc44b70a566a5010ea06b1f5db43885d8a2b9
Mohamed Hesham <moohisham2000@gmail.com>
2022-07-09 12:32:53 +0200

    portals

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,26 @@
+import classes from "./Modal.module.css";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const portalElem = document.getElementById("overlay");
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElem)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElem
+      )}
+    </Fragment>
+  );
+};
+export default Modal;
