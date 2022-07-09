9a3d703bd974e1bf7f6e97eaf669c577c703b080
Mohamed Hesham <moohisham2000@gmail.com>
2022-07-09 14:38:09 +0200

    prevent duplication in cart

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -8,13 +8,30 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if (action.type === "ADD_CART_ITEM") {
     //Update state in immutable way (Generate new array instead of editing previous one)
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-      return {
-        items:updatedItems,
-        totalAmount:updatedTotalAmount
-      }
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    //Prevent duplication  item/product
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
 };
 const CartProvider = (props) => {
