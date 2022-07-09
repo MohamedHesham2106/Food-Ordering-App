f7f604df8b6470c21d847d789768634c39e8bed2
Mohamed Hesham <moohisham2000@gmail.com>
2022-07-09 15:03:54 +0200

    add/remove cart buttons

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CardIcon from "../Cart/CartIcon";
 import CartContext from "../../context/cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  const [btnAnimation, setBtnAnimation] = useState(false);
   const ctx = useContext(CartContext);
-
+  const { items } = ctx;
   //reduce array to single number
-  const NumOfCartItems = ctx.items.reduce((curNumber, item) => {
+  const NumOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClass = `${classes.button} ${btnAnimation ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnAnimation(true);
+    //reset bump animation
+    const timer = setTimeout(() => {
+      setBtnAnimation(false);
+    }, 300);
+
+    //cleanUp fn
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClass} onClick={props.onClick}>
       <span className={classes.icon}>
         <CardIcon />
       </span>
--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,22 +1,24 @@
-import React, { useReducer } from "react";
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
 const defaultCartState = {
   items: [],
   totalAmount: 0,
 };
+
 const cartReducer = (state, action) => {
   if (action.type === "ADD_CART_ITEM") {
-    //Update state in immutable way (Generate new array instead of editing previous one)
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-    //Prevent duplication  item/product
+    //Prevent Duplication
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
+
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
@@ -33,24 +35,52 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === "REMOVE_CART_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    //Check if it is last item
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState;
 };
+
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
     cartReducer,
     defaultCartState
   );
+
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: "ADD_CART_ITEM", item: item });
   };
+
   const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: "REMOVE_CART_ITEM", id: id });
   };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
